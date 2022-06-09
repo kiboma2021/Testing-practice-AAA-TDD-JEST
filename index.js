e1d6470a8f32d235d789ce94cf7d6523d7a1e2a9
Ben Kiboma <omayioben@gmail.com>
2022-06-09 16:51:15 +0300

    Reverse string

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ class newString {
     let strlength=string.length;
       return strlength <= 10;
   }
+
+  reverseString(string) {
+    let reversedstr=string.split("").reverse().join("");
+      return reversedstr;
+  }
+
 }
 
 module.exports = newString;
--- index.test.js
+++ index.test.js
@@ -6,3 +6,11 @@ test('Find string length ', () => {
   let result = stringOne.stringLength(new_string);
   expect(result).toBe(true)
 })
+
+test('Find the reverse of a string ', () => {
+  const stringOne = new newString();
+  let new_string = "Microverse";
+  let reversedStr2 = new_string.split("").reverse().join("");
+  let result = stringOne.reverseString(new_string);
+  expect(result).toBe(reversedStr2)
+})
